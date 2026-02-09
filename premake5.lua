6531e7c44e0f033f50c9c04ef5c7119291e078d1
gh.jung <gh.jung@morph.kr>
2026-02-09 19:12:00 +0900

    base 뷰포트 동작 확인

--- premake5.lua
+++ premake5.lua
@@ -15,3 +15,7 @@ repo_build.prebuild_copy {
 }
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
+
+define_app("moprh.base.kit")
+define_app("morph.explorer.kit")
+define_app("morph.viewer.kit")
